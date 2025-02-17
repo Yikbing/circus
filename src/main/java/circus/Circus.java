f7875d7e33314a93f3120f22f05c1d2bcb968a78
Yikbing <E1156223@u.nus.edu>
2025-02-17 11:13:31 +0800

    more in lecture work haha update code to arraylist and add comparator to animal

--- src/main/java/circus/Circus.java
+++ src/main/java/circus/Circus.java
@@ -1,12 +1,12 @@
 package circus;
 
-import circus.animal.Animal;
-import circus.animal.Duck;
-import circus.animal.Parrot;
-import circus.animal.Tiger;
+import circus.animal.*;
 import circus.stuff.Cannon;
 import circus.stuff.Equipment;
 import circus.stuff.Ladder;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Circus {
     private static Animal[] animals = {
@@ -41,8 +41,35 @@ public class Circus {
     }
 
     public static void main(String[] args) {
-        makeAnimalsTalk();
+        System.out.println(animals.length);
+       /* makeAnimalsTalk();
         System.out.println("Total value of animals " + calculateAssetValue(animals));
         System.out.println("Total value of equipments " + calculateAssetValue(equipments));
+
+        animals[3] = new Elephant("Strong One");
+        System.out.println(animals.length);*/
+
+        ArrayList<Animal> animalArrayList = new ArrayList<>(Arrays.asList(animals));
+
+        animalArrayList.add(new Duck("Dora"));
+        Elephant darren = new Elephant("Darren");
+        animalArrayList.add(darren);
+
+        printAnimals(animalArrayList);
+
+        System.out.println("size of array list: " + animalArrayList.size()  );
+        System.out.println("darren is in position: " + animalArrayList.indexOf(darren));
+
+        animalArrayList.sort(Animal.AnimalNameComparator);
+
+        System.out.println("sorted array list: ");
+        printAnimals(animalArrayList);
     }
+
+    private static void printAnimals(ArrayList<Animal> animalArrayList) {
+        for (Animal a : animalArrayList) {
+            System.out.println(a);
+        }
+    }
+
 }
